c5fa1d4030dfbc9c8fe27b7e4f9194c05527a774
Sean Callaway <seancallaway@gmail.com>
2021-02-08 12:26:59 -0800

    Added custom user model.

--- subreddit_log/settings.py
+++ subreddit_log/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
