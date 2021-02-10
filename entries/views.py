388543c16b6e5b05395653bcbde47d393fd6699f
Sean Callaway <seancallaway@gmail.com>
2021-02-10 09:59:16 -0800

    Added base templates.

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LogView(TemplateView):
+    template_name = 'entries/log.html'
--- subreddit_log/settings.py
+++ subreddit_log/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'subreddit_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
