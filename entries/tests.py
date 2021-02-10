ed557ac6ed4d844e1728efb454b77c82bfecc36e
Sean Callaway <seancallaway@gmail.com>
2021-02-10 14:04:19 -0800

    Added test to ensure notes aren't visible when not logged in.

--- entries/tests.py
+++ entries/tests.py
@@ -25,6 +25,20 @@ class EntryFormTest(TestCase):
 
 class ProtectedViewsTest(TestCase):
 
+    def setUp(self) -> None:
+        self.note = 'This guy stinks on ice.'
+        rule1 = Rule.objects.create(name='Be civil')
+        Entry.objects.create(
+            user='thecal714',
+            rule=rule1,
+            action=Entry.ACTION_PERM_BAN,
+            notes=self.note,
+        )
+
+    def test_cannot_see_notes_as_anonymous(self):
+        response = self.client.get(reverse('log-view'))
+        self.assertNotContains(response, self.note)
+
     def test_cannot_access_add_entry_view_as_anonymous(self):
         response = self.client.get(reverse('entry-create'), follow=True)
         self.assertRedirects(response, f"{reverse('account_login')}?next={reverse('entry-create')}")
