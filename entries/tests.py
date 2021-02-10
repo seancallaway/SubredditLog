37bbf390e3719daf513b0af5e072fa05b8928f30
Sean Callaway <seancallaway@gmail.com>
2021-02-10 13:08:57 -0800

    Added test for EntryForm validation logic.

--- entries/tests.py
+++ entries/tests.py
@@ -1,1 +1,22 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+from entries.forms import EntryForm
+from entries.models import Entry, Rule
+
+
+class EntryFormTest(TestCase):
+
+    def setUp(self) -> None:
+        self.rule1 = Rule.objects.create(name='Be civil')
+
+    def test_cannot_submit_temp_ban_without_length(self):
+        form = EntryForm(data={
+            'user': 'thecal714',
+            'rule': self.rule1,
+            'action': Entry.ACTION_TEMP_BAN,
+        })
+
+        self.assertIn(
+            'Ban Length is required for temporary bans.',
+            form.errors['__all__']
+        )
